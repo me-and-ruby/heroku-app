2a12d27cc74354e6f0c2a5e7b314d9e20a927bc7
joash Nyabuti <nyabutijoash@yahoo.com>
2020-11-15 12:39:19 -0600

    added about page handler

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express=require('express');
 
 const app=express();
 
-var port=process.env.port || 3000;
+const PORT=process.env.PORT || 3000;
 
 app.get('/',function(req,res){
     res.send("<h1>Hello there</h1>")
 })
+app.get('/about',function(req,res){
+    res.send("<h1>About Page here</h1>")
+})
 
 
-app.listen(port,function(){
+app.listen(PORT,function(){
     
 })
